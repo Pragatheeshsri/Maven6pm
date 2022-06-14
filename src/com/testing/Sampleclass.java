cf62a8d99129b1345d33cf237d716199be9c8674
Pragatheeshsri <pragatheeshayyappan26@gmail.com>
2022-06-14 19:49:57 +0530

    Arun work is done

--- src/com/testing/Sampleclass.java
+++ src/com/testing/Sampleclass.java
@@ -2,7 +2,13 @@ package com.testing;
 
 public class Sampleclass {
 
+private void arun() {
 
+	System.out.println("This is Areun here...");
+	
+}
+	
+	
 	public static void main(String[] args) {
 		System.out.println("GIT");
 	}
