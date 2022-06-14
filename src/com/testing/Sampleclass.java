3b7383f7efa0335fbbbea0f194bce849f6ee24ed
Pragatheeshsri <pragatheeshayyappan26@gmail.com>
2022-06-14 20:03:16 +0530

    this is same

--- src/com/testing/Sampleclass.java
+++ src/com/testing/Sampleclass.java
@@ -2,15 +2,12 @@ package com.testing;
 
 public class Sampleclass {
 
+	public void sam() {
+		System.out.println("Same here....");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("GIT");
 	}
-		
-	
-	
-	
-	
-	
 
 }
