2d7e176c73ba0d9001131dc997724c911fcddc78
Pragatheeshsri <pragatheeshayyappan26@gmail.com>
2022-06-14 20:23:13 +0530

    this is Vinoth

--- src/com/testing/Sampleclass.java
+++ src/com/testing/Sampleclass.java
@@ -2,6 +2,10 @@ package com.testing;
 
 public class Sampleclass {
 
+	private void vinoth() {
+
+		System.out.println("Vinoth here....");
+	}
 
 	public static void main(String[] args) {
 		System.out.println("GIT");
